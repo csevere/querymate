db42f48086fae4010b238db85fa763a4076bc63c
krusnikStudent <cssdoitwell@gmail.com>
2019-05-03 17:53:24 -0400

    finished draft designs

--- src/Components/AuthCard.js
+++ src/Components/AuthCard.js
@@ -6,7 +6,8 @@ const AuthCard = (props) => {
     return(
         <div className = "grid-auth-container">
 
-            <div className="card grid-item-auth">
+            <div className="card grid-item-auth z-depth-3">
+            
                 <div className="card-content card-nav">
                     <nav className="pink darken-4">
                         <div className="nav-wrapper">
@@ -18,10 +19,45 @@ const AuthCard = (props) => {
                     </nav>
                 </div>
 
+                <div className="card-content panel1">
 
-                <div className="card-content"></div>
-                <div className="card-content"></div>
-                <div className="card-content"></div>
+                    <row className="card-smedia-wrapper">
+                        <button className="btn-large waves-effect waves-light pink darken-4 card-smedia-btn">
+                            <div><img className="card-smedia-img" src="images/google.png" alt=""/></div>
+                            <div className="card-smedia-text">Continue</div>
+                        </button>
+                        <button className="btn-large waves-effect waves-light pink darken-4 card-smedia-btn">
+                            <div><img className ="card-smedia-img" src="images/twitter.png" alt=""/></div>
+                            <div className="card-smedia-text">Continue</div>
+                        </button>
+                    </row>
+
+                    <row className="card-option center">
+                        <div className="center">Or login with email</div>
+                        <hr/>
+                    </row>
+
+                    <row className="card-email-wrapper">
+                        <div className="input-field">
+                            <input id="email" type="email" className="validate" />
+                            <label for="email"><i class="material-icons right">email</i>Email</label>
+                        </div>
+                        <div className="input-field">
+                            <input id="password" type="password" className="validate" />
+                            <label for="password"><i class="material-icons right">lock</i>Password</label>
+                        </div>
+                    </row>
+
+                    <div className="card-submit-wrapper">
+                        <button className="btn-large waves-effect waves-light pink darken-4 card-submit-btn" type="submit" name="action">
+                            <div className="card-submit-text">Log in <i class="material-icons right">send</i></div>
+                        </button>
+                    </div>
+
+
+                </div>
+
+                <div className="card-content panel2"></div>
             
             </div>
          
